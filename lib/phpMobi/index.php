72aeace371cab3d056ac9fb5696db3f45712de4c
soderlind <per@soderlind.no>
2016-07-22 15:07:34 +0200

    Update phpMobi to latest version

--- lib/phpMobi/index.php
+++ lib/phpMobi/index.php
@@ -10,46 +10,46 @@ if(isset($_GET["download"])){
 		//your eBook reader)
 		$url = "http://thetricky.net/mySQL/GROUP%20BY%20vs%20ORDER%20BY";
 		$recognize = false;
-	
+
 		//Create the mobi object
 		$mobi = new MOBI();
-	
+
 		//Find and set the content handler
 		$content = null;
-	
+
 		if($recognize){				//Pass through to right handler
 			$content = RecognizeURL::GetContentHandler($url);
 		}
-	
+
 		if($content == null){		//If handler not found or if the recognition was turned off
 			$content = new OnlineArticle($url);
 		}
-	
+
 		$mobi->setContentProvider($content);
-	
+
 		//Get title and make it a 12 character long filename
 		$title = $mobi->getTitle();
 		if($title === false) $title = "file";
 		$title = urlencode(str_replace(" ", "_", strtolower(substr($title, 0, 12))));
-	
+
 		//Send the mobi file as download
 		$mobi->download($title.".mobi");
 		die;
 	}else{
 		//Create the mobi object
 		$mobi = new MOBI();
-		
+
 		$content = new MOBIFile();
-		
+
 		$content->set("title", "My first eBook");
 		$content->set("author", "Me");
-		
+
 		$content->appendChapterTitle("Introduction");
 		for($i = 0, $lenI = rand(5, 10); $i < $lenI; $i++){
 			$content->appendParagraph("P".($i+1));
 		}
-		
-		
+
+
 		//Based on PHP's imagecreatetruecolor help paage
 		$im = imagecreatetruecolor(220, 200);
 		$text_color = imagecolorallocate($im, 233, 14, 91);
@@ -59,26 +59,26 @@ if(isset($_GET["download"])){
 		imagestring($im, 2, 10, 155,  'A Simple Text String', $text_color);
 		$content->appendImage($im);
 		imagedestroy($im);
-		
+
 		$content->appendPageBreak();
-		
+
 		for($i = 0, $lenI = rand(10, 15); $i < $lenI; $i++){
 			$content->appendChapterTitle(($i+1).". Chapter ".($i+1));
-			
+
 			for($j = 0, $lenJ = rand(20, 40); $j < $lenJ; $j++){
 				$content->appendParagraph("P".($i+1).".".($j+1)." TEXT TEXT TEXT");
 			}
-			
+
 			$content->appendPageBreak();
-		} 
-	
+		}
+
 		$mobi->setContentProvider($content);
-	
+
 		//Get title and make it a 12 character long filename
 		$title = $mobi->getTitle();
 		if($title === false) $title = "file";
 		$title = urlencode(str_replace(" ", "_", strtolower(substr($title, 0, 12))));
-	
+
 		//Send the mobi file as download
 		$mobi->download($title.".mobi");
 		die;
